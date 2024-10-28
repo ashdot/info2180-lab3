573f16cc4dbf2714874efe12c02bbaeefaf7e987
Ashle Johnson <ashlerose101@gmail.com>
2024-10-27 21:13:38 -0500

    edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,24 +7,28 @@ let current_player = player[0]
 //var status = document.getElementById('status')
 var btn = document.getElementsByClassName('btn')
 
-//const winning =  [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
+const winning =  [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
 
 const squares = document.querySelectorAll("#board div");
 
 
-
 for (let i = 0; i < squares.length; i++) {
     squares[i].classList.add("square")
-    //squares[i].addEventListener("click", function(){
-        //squares[i].textContent = X;
-        //squares[i].classList.add('X');
-        //squares[i].innerHTML = current_player;
-        //squares[i].classList.add(current_player);
+    squares[i].addEventListener("click", function(){
 
-        //current_player = current_player === 'X' ? 'O' : 'X';
+        if (!squares[i].textContent) {
+        squares[i].textContent = current_player;
+        squares[i].classList.add(current_player);
+        game_history.push({ player: current_player, square: i });
+        current_player = current_player === 'X' ? 'O' : 'X';
+        
+        }
 
 
-    //})
+
+
+
+    })
 
     //squares[i].addEventListener('mouseenter', () => {
         //squares[i].classList.add("hover");
